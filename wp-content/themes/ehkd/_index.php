95fea7d0989bbe09dbd2036e5d357c73424fd9e5
kaiyu li <krisfk@gmail.com>
2021-02-27 15:16:58 +0800

    .

--- wp-content/themes/ehkd/_index.php
+++ wp-content/themes/ehkd/_index.php
@@ -15,6 +15,23 @@
  */
 
 get_header();
+if ( have_posts() ) {
 
-echo 123;
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+
+		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
+	}
+
+	// Previous/next page navigation.
+	twenty_twenty_one_the_posts_navigation();
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content-none' );
+
+}
+
 get_footer();
