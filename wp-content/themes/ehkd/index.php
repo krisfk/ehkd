6718ab3eda300e56a4cbae6b12c55313d92e1cc5
kaiyu li <krisfk@gmail.com>
2021-02-28 00:10:38 +0800

    .

--- wp-content/themes/ehkd/index.php
+++ wp-content/themes/ehkd/index.php
@@ -111,16 +111,16 @@ get_header();
     <div class="row">
 
         <div class="col-3">
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-1.jpg" alt="">" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-1.jpg" alt="">
         </div>
         <div class="col-3">
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-2.jpg" alt="">" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-2.jpg" alt="">
         </div>
         <div class="col-3">
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-3.jpg" alt="">" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-3.jpg" alt="">
         </div>
         <div class="col-3">
-            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-4.jpg" alt="">" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-4.jpg" alt="">
         </div>
 
     </div>
