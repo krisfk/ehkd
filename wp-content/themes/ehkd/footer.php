c6193cd9c8d4f7ceba6e7ecb9e4fc81c88e71fea
kaiyu li <krisfk@gmail.com>
2021-02-28 16:41:48 +0800

    .

--- wp-content/themes/ehkd/footer.php
+++ wp-content/themes/ehkd/footer.php
@@ -16,7 +16,7 @@
 </div><!-- #primary -->
 
 
-<div class="copyright-div">Copyright &copy; Hong Kong Digital Asset Credit Limited All Rights Reserved.</div>
+<div class="copyright-div mt-3">Copyright &copy; Hong Kong Digital Asset Credit Limited All Rights Reserved.</div>
 
 </div><!-- #content -->
 
