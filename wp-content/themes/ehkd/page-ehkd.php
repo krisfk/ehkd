53f6a8fff49bc5e6c290800592021f4e8eb3f68e
kaiyu li <krisfk@gmail.com>
2021-03-01 02:11:15 +0800

    .

--- wp-content/themes/ehkd/page-ehkd.php
+++ wp-content/themes/ehkd/page-ehkd.php
@@ -35,7 +35,7 @@ get_header();
             </div>
         </div>
     </div>
-    <img src="<?php echo get_template_directory_uri();?>/assets/images/page-about-banner.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();?>/assets/images/page-ehkd-banner.jpg" alt="">
 </div>
 
 <div class="container mt-5">
