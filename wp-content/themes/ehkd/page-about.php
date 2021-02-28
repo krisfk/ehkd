97f70e2160be688550d2c31bfd54c1df11d11a9b
kaiyu li <krisfk@gmail.com>
2021-02-28 17:09:57 +0800

    .

--- wp-content/themes/ehkd/page-about.php
+++ wp-content/themes/ehkd/page-about.php
@@ -28,7 +28,7 @@ get_header();
         <div class="inner-msg-div-outer">
             <div class="inner-msg-div container">
 
-                <h1 class="banner-title">關於我們</h1>
+                <h1 class="banner-title mt-5">關於我們</h1>
 
 
 
