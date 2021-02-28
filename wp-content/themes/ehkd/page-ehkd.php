b5aa543e82d8e8d5c6f414c496921e5d9df394cf
kaiyu li <krisfk@gmail.com>
2021-03-01 02:04:37 +0800

    .

--- wp-content/themes/ehkd/page-ehkd.php
+++ wp-content/themes/ehkd/page-ehkd.php
@@ -28,7 +28,7 @@ get_header();
         <div class="inner-msg-div-outer">
             <div class="inner-msg-div container">
 
-                <h1 class="banner-title mt-5">關於我們</h1>
+                <h1 class="banner-title mt-5">易用錢 EHKD</h1>
 
 
 
