145a334568e56a1764a43baab0f8876cc9aec0d3
kaiyu li <krisfk@gmail.com>
2021-03-01 03:57:05 +0800

    .

--- wp-content/themes/ehkd/header.php
+++ wp-content/themes/ehkd/header.php
@@ -67,7 +67,7 @@
                                                 href="<?php echo get_site_url();?>/special-personal-loan-program-for-civil-servant">公務員低息貸款</a>
                                         </li>
                                         <li>
-                                            <a href="#">物業貸款</a>
+                                            <a href="<?php echo get_site_url();?>/mortgage-loan">物業貸款</a>
                                         </li>
                                         <li>
                                             <a
