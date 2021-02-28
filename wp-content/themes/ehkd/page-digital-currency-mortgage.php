ee4077fa8cfe9ca969fb6fe7abfe1a7a669676c4
kaiyu li <krisfk@gmail.com>
2021-03-01 03:35:09 +0800

    .

--- wp-content/themes/ehkd/page-digital-currency-mortgage.php
+++ wp-content/themes/ehkd/page-digital-currency-mortgage.php
@@ -64,22 +64,17 @@ get_header();
 
     <div class="row mt-5">
 
-        <div class="col-lg-3"> <img class="home-point-icon"
-                src="http://104.248.237.5/wp-content/themes/ehkd/assets/images/home-icon-1.png" alt="">
-            <div class="text-center home-point-txt">獨家貸款評估 <br>
-                <span class="small">(免查TU信貸評級)</span>
-            </div>
-        </div>
-        <div class="col-lg-3"><img class="home-point-icon"
+
+        <div class="col-lg-4"><img class="home-point-icon"
                 src="http://104.248.237.5/wp-content/themes/ehkd/assets/images/home-icon-2.png" alt="">
             <div class="text-center home-point-txt">最快15分鐘批核</div>
         </div>
-        <div class="col-lg-3"><img class="home-point-icon"
+        <div class="col-lg-4"><img class="home-point-icon"
                 src="http://104.248.237.5/wp-content/themes/ehkd/assets/images/home-icon-3.png" alt="">
             <div class="text-center home-point-txt">靈活慳息 <br>
                 利息逐日計</div>
         </div>
-        <div class="col-lg-3"><img class="home-point-icon"
+        <div class="col-lg-4"><img class="home-point-icon"
                 src="http://104.248.237.5/wp-content/themes/ehkd/assets/images/home-icon-4.png" alt="">
 
             <div class="text-center home-point-txt">現金即到手</div>
