8d9bbc5a1576b4e987f7411466c5e5f97b665435
kaiyu li <krisfk@gmail.com>
2021-02-28 17:01:36 +0800

    .

---
+++ wp-content/themes/ehkd/page-about.php
@@ -0,0 +1,227 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+
+
+<div class="main-banner">
+
+    <div class="home-main-banner-inner-div">
+
+
+        <div class="inner-msg-div-outer">
+            <div class="inner-msg-div container">
+
+                <div class="inner-msg-div-inner-div">
+
+                    <img class="home-banner-innner-img"
+                        src="<?php echo get_template_directory_uri();?>/assets/images/home-banner-innner-img.png"
+                        alt="">
+
+                    <div class="mt-5 text-center"> <a href="javascript:void(0);" class="apply-now-btn">立即申請</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img src="<?php echo get_template_directory_uri();?>/assets/images/home-banner.jpg" alt="">
+</div>
+
+<div class="container mt-5">
+
+
+    <div class="row">
+
+        <div class="col-lg-3"> <img class="home-point-icon"
+                src="<?php echo get_template_directory_uri();?>/assets/images/home-icon-1.png" alt="">
+            <div class="text-center home-point-txt">獨家貸款評估 <br>
+                <span class="small">(免查TU信貸評級)</span>
+            </div>
+        </div>
+        <div class="col-lg-3"><img class="home-point-icon"
+                src="<?php echo get_template_directory_uri();?>/assets/images/home-icon-2.png" alt="">
+            <div class="text-center home-point-txt">最快15分鐘批核</div>
+        </div>
+        <div class="col-lg-3"><img class="home-point-icon"
+                src="<?php echo get_template_directory_uri();?>/assets/images/home-icon-3.png" alt="">
+            <div class="text-center home-point-txt">靈活慳息 <br>
+                利息逐日計</div>
+        </div>
+        <div class="col-lg-3"><img class="home-point-icon"
+                src="<?php echo get_template_directory_uri();?>/assets/images/home-icon-4.png" alt="">
+
+            <div class="text-center home-point-txt">現金即到手</div>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- <div class="container"> -->
+
+<div class="row mt-5">
+
+    <div class="col-3 g-0">
+        <img class="rect-img" src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-1.jpg" alt="">
+        <div class="rect-img-txt">業主私人貸款</div>
+    </div>
+    <div class="col-3 g-0">
+        <img class="rect-img" src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-2.jpg" alt="">
+
+        <div class="rect-img-txt">公務員低息貨款</div>
+    </div>
+    <div class="col-3 g-0">
+        <img class="rect-img" src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-3.jpg" alt="">
+        <div class="rect-img-txt">物業貸款</div>
+    </div>
+    <div class="col-3 g-0">
+        <img class="rect-img" src="<?php echo get_template_directory_uri();?>/assets/images/home-rect-img-4.jpg" alt="">
+        <div class="rect-img-txt">數字貨幣抵押貸款</div>
+    </div>
+
+</div>
+
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-lg-4">
+
+            <div class="footer-lightblue-title mt-5">立即申請</div>
+
+            <div class="footer-apply-btn-above-txt mt-3">您可透過網上或客戶服務熱線進行貸款申請。康業為您提供特快申請：15分鐘初步審批，24小時過數。</div>
+
+            <a href="#" class="footer-apply-btn mt-4">網上申請</a>
+        </div>
+        <div class="col-lg-8">
+
+
+            <div class=" footer-sitemap mt-5">
+                <div class="footer-sitemap-branch">
+
+                    <ul>
+                        <li>
+                            <h2>貸款產品</h2>
+                        </li>
+                        <li><a href="#">數字貨幣抵押貸款</a></li>
+                        <li><a href="#">易用錢</a></li>
+                        <li><a href="#">物業貸款</a></li>
+                        <li><a href="#">業主私人貸款</a></li>
+                        <li><a href="#">公務員低息貸款</a></li>
+                    </ul>
+
+
+
+
+
+
+
+                </div>
+                <div class="footer-sitemap-branch">
+
+                    <ul>
+                        <li>
+                            <h2> 公司資訊</h2>
+                        </li>
+                        <li><a href="#">關於我們</a></li>
+                        <li><a href="#">媒體</a></li>
+                        <li><a href="#">最新資訊</a></li>
+                        <li><a href="#">常見問題</a></li>
+                    </ul>
+
+
+
+
+                </div>
+                <div class="footer-sitemap-branch">
+
+                    <ul>
+                        <li>法律聲明</li>
+                        <li>免責條款</li>
+                        <li>私隱聲明</li>
+                        <li>貸款申請條款</li>
+                    </ul>
+                </div>
+                <div class="footer-sitemap-branch">
+                    <ul>
+
+                        <li>
+                            <h2> 放債人牌照號碼 </h2>
+                        </li>
+                        <li> 香港數字資產信貸有限公司：168/2021
+                        </li>
+                    </ul>
+
+
+
+
+
+
+
+                </div>
+
+
+                <div class="text-start mt-4">
+
+
+                    <div class="d-inline-block me-4 ">
+
+                        <span class="footer-phone footer-phone-1"> 2628 2828</span>
+
+                        <div class="footer-under-phone-txt">忠告：借錢梗要還，咪俾錢中介</div>
+
+
+                    </div>
+                    <div class="d-inline-block">
+
+                        <span class="footer-phone footer-phone-2"> 5700 8888</span>
+                        <div class="footer-under-phone-txt">投訴熱線：2523 7861</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<!-- </div> -->
+</div>
+
+
+
+<script type="text/javascript">
+$(function() {
+
+    // $(window).load(function() {
+
+    // })
+})
+
+window.onload = function() {
+    $('.inner-msg-div-inner-div').height($('.inner-msg-div-outer').height()).css({
+        'opacity': '1'
+    });
+}
+</script>
+
+
+<?php
+get_footer();
