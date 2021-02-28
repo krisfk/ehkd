0b31164b43bf06af9fa491abb11498b1532e8054
kaiyu li <krisfk@gmail.com>
2021-03-01 04:28:29 +0800

    .

--- wp-content/themes/ehkd/page-loan-calculator.php
+++ wp-content/themes/ehkd/page-loan-calculator.php
@@ -35,7 +35,7 @@ get_header();
             </div>
         </div>
     </div>
-    <img src="<?php echo get_template_directory_uri();?>/assets/images/loan-cal-banner.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri();?>/assets/images/loancal-banner.jpg" alt="">
 </div>
 
 <div class="container mt-5">
