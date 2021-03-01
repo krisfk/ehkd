9cbf43d235990902d5e9479dbd928ef6b100fb5a
kaiyu li <krisfk@gmail.com>
2021-03-02 03:40:50 +0800

    .

--- wp-content/themes/ehkd/page-apply.php
+++ wp-content/themes/ehkd/page-apply.php
@@ -76,7 +76,7 @@ get_header();
                 <div class="row">
 
 
-                    <div class="col-lg-12 mb-3">
+                    <div class="col-lg-12 mb-5">
                         <label for="loan-type">貸款類型: </label>
 
                         <select name="loan-type" id="loan-type" class="form-select">
