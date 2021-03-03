ceaa9ab1646723f1f7c7d87dff2648460120636a
kaiyu li <krisfk@gmail.com>
2021-03-03 16:30:06 +0800

    .

--- wp-content/themes/ehkd/page-digital-currency-mortgage.php
+++ wp-content/themes/ehkd/page-digital-currency-mortgage.php
@@ -74,7 +74,7 @@ $img_src=$img_arr[0];
 
     <div class="row mt-5">
         <?php
-$img_id = get_field('banner');
+$img_id = get_field('icon_1');
 $img_arr = wp_get_attachment_image_src($img_id,'full');
 $img_src=$img_arr[0];
 ?>
@@ -86,7 +86,7 @@ $img_src=$img_arr[0];
             </div>
         </div>
         <?php
-$img_id = get_field('banner');
+$img_id = get_field('icon_2');
 $img_arr = wp_get_attachment_image_src($img_id,'full');
 $img_src=$img_arr[0];
 ?>
@@ -98,7 +98,7 @@ $img_src=$img_arr[0];
             </div>
         </div>
         <?php
-$img_id = get_field('banner');
+$img_id = get_field('icon_3');
 $img_arr = wp_get_attachment_image_src($img_id,'full');
 $img_src=$img_arr[0];
 ?>
