d9f60fab0c08a7fb2423892feb2211e2c7716d2e
kaiyu li <krisfk@gmail.com>
2021-03-03 15:42:45 +0800

    .

--- wp-content/themes/ehkd/page-special-personal-loan-program-for-civil-servant.php
+++ wp-content/themes/ehkd/page-special-personal-loan-program-for-civil-servant.php
@@ -28,14 +28,22 @@ get_header();
         <div class="inner-msg-div-outer">
             <div class="inner-msg-div container">
 
-                <h1 class="banner-title mt-5">公務員低息貸款</h1>
+                <h1 class="banner-title mt-5"><?php echo get_the_title();?></h1>
 
 
 
             </div>
         </div>
     </div>
-    <img src="<?php echo get_template_directory_uri();?>/assets/images/civil-loan-banner.jpg" alt="">
+
+    <?php
+$img_id = get_field('banner');
+$img_arr = wp_get_attachment_image_src($img_id,'full');
+$img_src=$img_arr[0];
+?>
+    <img src="<?php echo $img_src;?>" alt="">
+
+    <!-- <img src="<?php echo get_template_directory_uri();?>/assets/images/civil-loan-banner.jpg" alt=""> -->
 </div>
 
 <div class="container mt-5">
@@ -44,18 +52,20 @@ get_header();
 
     <div class="blue-title text-center">
 
-        短期周轉首選 <br>
-        貸款額最高 <span class="big"> 可達月薪5倍</span>
+        <!-- 短期周轉首選 <br>
+        貸款額最高 <span class="big"> 可達月薪5倍</span> -->
 
+        <?php echo get_field('content_1');?>
 
 
     </div>
 
     <div class="content-txt text-center mt-5">
 
-        一時手頭緊，又唔想整花個人信貸評級，影響前途? <br>
+        <!-- 一時手頭緊，又唔想整花個人信貸評級，影響前途? <br>
         不論你是公務員、醫護人員或老師，只需出示職員證件，毋須上傳環聯信貸紀錄也可申請貸款， <br>
-        更可享特低利息循環貸款，最快即日批核現金，周轉暢通無阻!
+        更可享特低利息循環貸款，最快即日批核現金，周轉暢通無阻! -->
+        <?php echo get_field('content_2');?>
 
     </div>
 
@@ -67,19 +77,30 @@ get_header();
             <!-- <div class="col-lg-6"> -->
 
             <div class="d-table-cell w-50">
-                <img class="w-100" src="<?php echo get_template_directory_uri();?>/assets/images/civil-loan-img.jpg"
-                    alt="">
+
+
+                <?php
+$img_id = get_field('image_1');
+$img_arr = wp_get_attachment_image_src($img_id,'full');
+$img_src=$img_arr[0];
+?>
+                <img class="w-100" src="<?php echo $img_src;?>" alt="">
+
+                <!-- <img class="w-100" src="<?php echo get_template_directory_uri();?>/assets/images/civil-loan-img.jpg"
+                    alt=""> -->
                 <!-- </div> -->
                 <!-- <div class="col-lg-6"> -->
 
             </div>
             <div class="d-table-cell w-50 align-middle">
-                <ul class=" big-point-ul">
+                <!-- <ul class=" big-point-ul">
 
                     <li>不損信貸評級</li>
                     <li>特快批核</li>
                     <li>利息特低</li>
-                </ul>
+                </ul> -->
+                <?php echo get_field('content_3');?>
+
 
 
             </div>
@@ -102,16 +123,17 @@ get_header();
 
     <div class="light-blue-inner-content text-center">
 
-        <div class="bold-title">申請條件</div>
+        <!-- <div class="bold-title">申請條件</div>
 
         <div class="txt">
             公務員/醫護人員特享低息 <br>
             年滿18歲香港永久居民 <br>
             最近3個月入息證明 <br>
             最近3個月的住址證明
-        </div>
+        </div> -->
+        <?php echo get_field('apply_requirement');?>
 
-        <a class="apply-btn" href="#">立即申請</a>
+        <a class="apply-btn" href="#"><?php echo get_field('apply_btn');?></a>
 
 
     </div>
