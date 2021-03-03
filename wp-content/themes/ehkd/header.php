02083edbe757fe06cbc783e3a492abb3379de6a5
kaiyu li <krisfk@gmail.com>
2021-03-04 04:27:46 +0800

    .

--- wp-content/themes/ehkd/header.php
+++ wp-content/themes/ehkd/header.php
@@ -80,10 +80,11 @@
                                 
                                 if(count($menu_item['children']))
                                 {
+                                    print_r($menu_item['children']);
+
                                     echo '<ul class="top-menu-submenu">';
                                     foreach ($menu_item['children'] as $sub_menu_item) 
                                     {
-                                        print_r($sub_menu_item);
                                         $sub_url = $sub_menu_item['url'];
                                         $sub_title = $sub_menu_item['title'];
                                         $sub_temp_arr=explode(get_site_url(),$sub_url);
