62156ddc03cfe3fe9632c73f6167dfdacb900d23
kaiyu li <krisfk@gmail.com>
2021-03-03 16:29:28 +0800

    .

--- wp-content/themes/ehkd/page-digital-currency-mortgage.php
+++ wp-content/themes/ehkd/page-digital-currency-mortgage.php
@@ -124,16 +124,20 @@ $img_src=$img_arr[0];
 
     <div class="light-blue-inner-content text-center">
 
-        <div class="bold-title">申請條件</div>
+        <?php echo get_field('apply_requirement');?>
+
+
+        <!-- <div class="bold-title">申請條件</div>
 
         <div class="txt">
             HKD.com注冊用戶 <br>
             年滿18歲香港永久居民 <br>
             最近3個月入息證明 <br>
             最近3個月的住址證明
-        </div>
+        </div> -->
 
-        <a class="apply-btn" href="#">立即申請</a>
+        <a class="apply-btn" href="#"> <?php echo get_field('apply_btn');?>
+        </a>
 
 
     </div>
