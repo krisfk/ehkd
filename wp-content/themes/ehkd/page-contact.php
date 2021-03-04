0a3d7038387ffbf1cf9103ada755725ac1b7dd42
kaiyu li <krisfk@gmail.com>
2021-03-05 04:19:09 +0800

    .

--- wp-content/themes/ehkd/page-contact.php
+++ wp-content/themes/ehkd/page-contact.php
@@ -66,7 +66,7 @@ $img_src=$img_arr[0];
                         <span class="blue-title">香港數字資產信貸有限公司</span>
                     </div>
                     <div class="col-6">
-                        <div><span class="blue-subtitle">放債人牌照號碼:</span>
+                        <div class="float-left"><span class="blue-subtitle">放債人牌照號碼:</span>
                             <span>1688/2021</span>
                         </div>
 
