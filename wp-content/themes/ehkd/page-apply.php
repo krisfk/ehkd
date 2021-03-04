c1fffdb41c00e88b6571cc85daaffce07f871f96
kaiyu li <krisfk@gmail.com>
2021-03-05 04:47:02 +0800

    .

--- wp-content/themes/ehkd/page-apply.php
+++ wp-content/themes/ehkd/page-apply.php
@@ -17,9 +17,7 @@
 get_header();
 
 ?>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 
 
