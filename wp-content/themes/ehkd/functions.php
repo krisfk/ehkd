9cde9e3316657ef2a43a9d90197cbcd3a66ef543
kaiyu li <krisfk@gmail.com>
2021-03-05 17:54:13 +0800

    .

--- wp-content/themes/ehkd/functions.php
+++ wp-content/themes/ehkd/functions.php
@@ -716,7 +716,40 @@ if ($post_id) {
 
 	add_post_meta($post_id, 'where_from', 'where_from');
 
-	add_post_meta($post_id, 'customer_ip', 'customer_ip');
+
+	# PHP7+
+	$clientIP = $_SERVER['HTTP_CLIENT_IP'] 
+	?? $_SERVER["HTTP_CF_CONNECTING_IP"] # when behind cloudflare
+	?? $_SERVER['HTTP_X_FORWARDED'] 
+	?? $_SERVER['HTTP_X_FORWARDED_FOR'] 
+	?? $_SERVER['HTTP_FORWARDED'] 
+	?? $_SERVER['HTTP_FORWARDED_FOR'] 
+	?? $_SERVER['REMOTE_ADDR'] 
+	?? '0.0.0.0';
+
+	# Earlier than PHP7
+	$clientIP = '0.0.0.0';
+
+	if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+	$clientIP = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+	# when behind cloudflare
+	$clientIP = $_SERVER['HTTP_CF_CONNECTING_IP']; 
+	} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	$clientIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} elseif (isset($_SERVER['HTTP_X_FORWARDED'])) {
+	$clientIP = $_SERVER['HTTP_X_FORWARDED'];
+	} elseif (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+	$clientIP = $_SERVER['HTTP_FORWARDED_FOR'];
+	} elseif (isset($_SERVER['HTTP_FORWARDED'])) {
+	$clientIP = $_SERVER['HTTP_FORWARDED'];
+	} elseif (isset($_SERVER['REMOTE_ADDR'])) {
+	$clientIP = $_SERVER['REMOTE_ADDR'];
+	}
+
+	// echo "My client IP: ", $clientIP;
+
+	add_post_meta($post_id, 'customer_ip', $clientIP);
 
 }
 
