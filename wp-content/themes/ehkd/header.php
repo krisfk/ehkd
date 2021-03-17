f7469a48ccd44b69a37f352cb0bf8f1d41757601
kaiyu li <krisfk@gmail.com>
2021-03-17 18:17:35 +0800

    .

--- wp-content/themes/ehkd/header.php
+++ wp-content/themes/ehkd/header.php
@@ -151,8 +151,7 @@ $langs= icl_get_languages('skip_missing=0&orderby=custom&order=asc&link_empty_to
                 <div class="lang-div">
                     <a href="<?php echo $langs['zh-hant']['url'];?>">繁</a><span class="sep">/</span><a
                         href="<?php echo $langs['cn']['url'];?>">簡</a>
-                    <!-- <span
-            class="sep">/</span><a href="<?php echo $langs['en']['url'];?>">ENGLISH</a> -->
+                    <span class="sep">/</span><a href="<?php echo $langs['en']['url'];?>">ENGLISH</a>
 
                 </div>
             </li>
