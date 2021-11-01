ec8bd2b8d9d7fb189c737d97b5f541efc4606b70
kaiyu li <krisfk@gmail.com>
2021-11-02 03:59:59 +0800

    .

--- gen_captcha.php
+++ gen_captcha.php
@@ -4,5 +4,6 @@ include("simple-php-captcha.php");
 $_SESSION['captcha'] = simple_php_captcha();
 $captcha_url = $_SESSION['captcha']['image_src'];
 $captcha_code = $_SESSION['captcha']['code'];
+
 echo $captcha_url;
 ?>
